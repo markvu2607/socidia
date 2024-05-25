68f25f2e9b1376a691bf1c31e44ee129f974ed46
markvu2607 <markvu.work@gmail.com>
2024-05-25 15:56:08 +0700

    ✨ feat: setup auth with clerk

--- src/feat/common/components/icons/index.ts
+++ src/feat/common/components/icons/index.ts
@@ -1,3 +1,7 @@
 export * from "./home-icon";
 export * from "./group-icon";
 export * from "./plus-circle-icon";
+export * from "./user-circle-icon";
+export * from "./chat-icon";
+export * from "./bell-icon";
+export * from "./search-icon";
--- src/feat/common/components/layouts/nav-bar.tsx
+++ src/feat/common/components/layouts/nav-bar.tsx
@@ -1,10 +1,22 @@
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
 
 import {
+  BellIcon,
+  ChatIcon,
   GroupIcon,
   HomeIcon,
   PlusCircleIcon,
+  SearchIcon,
+  UserCircleIcon,
 } from "@/feat/common/components/icons";
+import { cn } from "@/lib/utils";
 import { MobileMenu } from "./mobile-menu";
 
 type Props = {};
@@ -17,7 +29,7 @@ export const NavBar = (props: Props) => {
           Socidia
         </Link>
       </div>
-      <div className="hidden md:flex w-1/2 text-sm">
+      <div className="hidden md:flex w-1/2 text-sm items-center justify-between">
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex gap-2">
             <HomeIcon className="size-6" />
@@ -33,7 +45,40 @@ export const NavBar = (props: Props) => {
           </Link>
         </div>
       </div>
+      <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+        <input
+          type="text"
+          placeholder="search..."
+          className="bg-transparent outline-none"
+        />
+        <SearchIcon className="size-6" />
+      </div>
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div
+            className={cn(
+              "w-8 h-8",
+              "inline-block",
+              "rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface dark:text-white",
+              "animate-spin motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            )}
+            role="status"
+          />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <GroupIcon className="size-6" />
+            <ChatIcon className="size-6" />
+            <BellIcon className="size-6" />
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <UserCircleIcon className="size-6" />
+              <Link href="/sign-in">Login</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
