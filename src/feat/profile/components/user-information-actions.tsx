6a1eb2c45b92122d8e243f2710325f7f764bdd91
markvu2607 <markvu.work@gmail.com>
2024-06-04 14:11:59 +0700

    ✨ feat: implement follow action

--- src/feat/profile/components/user-information-actions.tsx
+++ src/feat/profile/components/user-information-actions.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+import { switchFollow } from "@/feat/profile/actions";
 
 type Props = {
   userId: string;
@@ -15,20 +19,37 @@ const UserInformationActions = ({
   isUserBlocked,
   userId,
 }: Props) => {
+  const [userState, setUserState] = useState({
+    following: isFollowing,
+    blocked: isUserBlocked,
+    followingRequestSent: isFollowingSent,
+  });
+
+  const follow = async () => {
+    try {
+      await switchFollow(userId);
+      setUserState((prev) => ({
+        ...prev,
+        following: prev.following && false,
+        followingRequestSent:
+          !prev.following && !prev.followingRequestSent ? true : false,
+      }));
+    } catch (error) {}
+  };
   return (
     <>
-      <form action="">
+      <form action={follow}>
         <button className="bg-blue-500 text-white text-sm rounded-md p-2">
-          {isFollowing
+          {userState.following
             ? "Following"
-            : isFollowingSent
+            : userState.followingRequestSent
             ? "Friend Request Sent"
             : "Follow"}
         </button>
       </form>
       <form action="" className="self-end">
         <span className="text-red-400 text-xs cursor-pointer">
-          {isUserBlocked ? "Unblock User" : "Block User"}
+          {userState.blocked ? "Unblock User" : "Block User"}
         </span>
       </form>
     </>
