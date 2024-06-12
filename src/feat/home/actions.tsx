38e97dbcad1d4e51dd9c0c301caf2bd52ecb8dfa
markvu2607 <markvu.work@gmail.com>
2024-06-12 13:08:27 +0700

    ✨ feat: implement add post

--- src/feat/home/actions.tsx
+++ src/feat/home/actions.tsx
@@ -1,6 +1,8 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
 import prisma from "@/lib/prisma";
 
@@ -58,5 +60,35 @@ export const addComment = async (postId: number, desc: string) => {
   } catch (err) {
     console.log(err);
     throw new Error("Something went wrong!");
+  }
+};
+
+export const addPost = async (formData: FormData, img: string) => {
+  const desc = formData.get("desc") as string;
+
+  const Desc = z.string().min(1).max(255);
+
+  const validatedDesc = Desc.safeParse(desc);
+
+  if (!validatedDesc.success) {
+    console.log("description is not valid");
+    return;
+  }
+  const { userId } = auth();
+
+  if (!userId) throw new Error("User is not authenticated!");
+
+  try {
+    await prisma.post.create({
+      data: {
+        desc: validatedDesc.data,
+        userId,
+        img,
+      },
+    });
+
+    revalidatePath("/");
+  } catch (err) {
+    console.log(err);
   }
 };
--- src/feat/home/components/add-post.tsx
+++ src/feat/home/components/add-post.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useState } from "react";
+
 import {
   CalendarIcon,
   EmojiIcon,
@@ -5,36 +12,66 @@ import {
   PlayIcon,
   PollIcon,
 } from "@/feat/common/components/icons";
-import Image from "next/image";
-import React from "react";
+import { AddPostButton } from "@/feat/home/components/add-post-button";
+import { addPost } from "@/feat/home/actions";
 
 type Props = {};
 
 export const AddPost = (props: Props) => {
+  const { user, isLoaded } = useUser();
+  const [desc, setDesc] = useState("");
+  const [img, setImg] = useState<any>();
+
+  if (!isLoaded) {
+    return "Loading ...";
+  }
+
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       <Image
-        src="https://images.unsplash.com/photo-1724232547374-69758574fff5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
+        src={user?.imageUrl || "default-avatar.jpg"}
         alt=""
         width={48}
         height={48}
         className="w-12 h-12 object-cover rounded-full"
       />
       <div className="flex-1">
-        <div className="flex gap-4 ">
+        <form
+          action={(formData) => addPost(formData, img?.secure_url || "")}
+          className="flex gap-4"
+        >
           <textarea
-            name=""
+            name="desc"
             id=""
             placeholder="What's on your mind?"
             className="bg-slate-100 rounded-e-lg flex-1 p-2"
+            onChange={(e) => setDesc(e.target.value)}
           ></textarea>
-          <EmojiIcon className="size-6 self-end" />
-        </div>
-        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
-          <div className="flex items-center gap-2">
-            <PhotoIcon className="size-6" />
-            <span>Photo</span>
+          <div>
+            <EmojiIcon className="size-6 self-end" />
+            <AddPostButton />
           </div>
+        </form>
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <CldUploadWidget
+            uploadPreset="socidia"
+            onSuccess={(result, { widget }) => {
+              setImg(result.info);
+              widget.close();
+            }}
+          >
+            {({ open }) => {
+              return (
+                <div
+                  className="flex items-center gap-2 cursor-pointer"
+                  onClick={() => open()}
+                >
+                  <PhotoIcon className="size-6" />
+                  <span>Photo</span>
+                </div>
+              );
+            }}
+          </CldUploadWidget>
           <div className="flex items-center gap-2">
             <PlayIcon className="size-6" />
             <span>Video</span>
